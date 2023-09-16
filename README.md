8af4eab4f148a39c0354ce76ca6bef5938985bec
Anzz <makrufmustofa79@gmail.com>
2023-09-16 08:55:46 +0700

    v.1

---
+++ README.md
@@ -0,0 +1,83 @@
+# Welcome to Backend UMKM
+
+API ini berisi list produk dan search produk
+
+## Installation
+
+Run the following command to clone the repository, and install the dependencies.
+
+```sh
+git clone https://github.com/annalitcz/backend-umkm
+cd backend-umkm
+pip install -r requirement.txt
+```
+
+start the server
+
+```sh
+python app.py
+```
+
+## Response
+
+- get all product:
+
+  ```link
+  /api/products
+  ```
+
+  Data:
+
+  ```json
+  {
+    "message": "API produk berhasil diakses.",
+    "products": [
+      {
+        "deskripsi": "Deskripsi Produk 1",
+        "gambar": "/assets/image_2023-08-06_23-20-36.png",
+        "harga": 100.0,
+        "nama": "Produk 1"
+      },
+      {
+        "deskripsi": "Deskripsi Produk 2",
+        "gambar": "/assets/image_2023-08-06_23-20-36.png",
+        "harga": 150.0,
+        "nama": "Produk 2"
+      },
+      {
+        "deskripsi": "Deskripsi Produk 3",
+        "gambar": "/assets/image_2023-08-06_23-20-36.png",
+        "harga": 50.0,
+        "nama": "Produk 3"
+      }
+    ],
+    "status": "Success"
+  }
+  ```
+
+- Search Products
+
+  ```link
+  /api/products/search?query=produk%201
+  ```
+
+  Data:
+
+  ```json
+  {
+    "message": "Hasil pencarian produk",
+    "products": [
+      {
+        "deskripsi": "Deskripsi Produk 2",
+        "gambar": "/assets/image_2023-08-06_23-20-36.png",
+        "harga": 150.0,
+        "nama": "Produk 2"
+      }
+    ],
+    "status": "Success"
+  }
+  ```
+
+## Support me
+
+<button name="button" style="width:150px; background: red; color: white; padding: 5px; border-radius: 5px; font-weight: bold" onclick='https://saweria.co/AnzzID'>Saweria</button>
---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, jsonify, request
+import json
+
+app = Flask(__name__)
+
+products = [
+    {
+        "nama": "Produk 3",
+        "gambar": "/assets/image_2023-08-06_23-20-36.png",
+        "deskripsi": "Deskripsi Produk 3",
+        "harga": 50.0
+    },
+    {
+        "nama": "Produk 1",
+        "gambar": "/assets/image_2023-08-06_23-20-36.png",
+        "deskripsi": "Deskripsi Produk 1",
+        "harga": 100.0
+    },
+    {
+        "nama": "Produk 2",
+        "gambar": "/assets/image_2023-08-06_23-20-36.png",
+        "deskripsi": "Deskripsi Produk 2",
+        "harga": 150.0
+    },
+    # Tambahkan produk lainnya...
+]
+
+@app.route('/')
+def home():
+    return('<div style="display: flex; flex-direction: column; justify-content: center; align-items: center"><h1 style="text-align:center;">Backend UMKM</h1><p>ini adalah contoh backend sederhana dengan python Flask</p></div>')
+@app.route('/api/products', methods=['GET'])
+def get_all_products():
+    sorted_products = sorted(products, key=lambda x: x['nama'])  # Mengurutkan produk berdasarkan nama
+    response = {
+        "status": "Success",
+        "message": "API produk berhasil diakses.",
+        "products": sorted_products
+    }
+    return jsonify(response)
+
+@app.route('/api/products/search', methods=['GET'])
+def search_products():
+    query = request.args.get('query', '').lower()  # Mengambil parameter pencarian dari URL
+
+    # Mencari produk yang sesuai dengan query
+    matching_products = [product for product in products if query in product['nama'].lower()]
+
+    sorted_matching_products = sorted(matching_products, key=lambda x: x['nama'])  # Mengurutkan hasil pencarian
+
+    response = {
+        "status": "Success",
+        "message": "Hasil pencarian produk",
+        "products": sorted_matching_products
+    }
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
